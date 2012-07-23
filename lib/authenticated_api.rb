c0dd312ff4e9610216a9e06155eef3cfdd95954c
Christoph Geschwind <christoph@mixxt.net>
2012-07-23 12:27:25 +0000

    refactored Headers and Driver loading

--- lib/authenticated_api.rb
+++ lib/authenticated_api.rb
@@ -1,9 +1,12 @@
 require 'openssl'
 require 'base64'
 
+require 'authenticated_api/errors'
+require 'authenticated_api/helpers'
+require 'authenticated_api/base'
+
 module AuthenticatedApi
-  require 'authenticated_api/errors'
-  require 'authenticated_api/helpers'
+  autoload :Headers, 'authenticated_api/headers'
 
   module RequestDrivers
     autoload :NetHttpRequest, 'authenticated_api/request_drivers/net_http'
@@ -11,8 +14,6 @@ module AuthenticatedApi
     autoload :RestClientRequest, 'authenticated_api/request_drivers/rest_client'
   end
 
-  require 'authenticated_api/headers'
-  require 'authenticated_api/base'
 end
 
 
--- lib/authenticated_api/headers.rb
+++ lib/authenticated_api/headers.rb
@@ -3,26 +3,16 @@ module AuthenticatedApi
   # Builds the canonical string given a request object.
   class Headers
     
-    include RequestDrivers
-    
     def initialize(request)
       @original_request = request
       
       case request.class.to_s
       when /Net::HTTP/
-        @request = NetHttpRequest.new(request)
+        @request = RequestDrivers::NetHttpRequest.new(request)
       when /RestClient/
-        @request = RestClientRequest.new(request)
+        @request = RequestDrivers::RestClientRequest.new(request)
       when /Curl::Easy/
-        @request = CurbRequest.new(request)
-      when /ActionController::Request/
-        @request = ActionControllerRequest.new(request)
-      when /ActionController::TestRequest/
-        @request = ActionControllerRequest.new(request)
-        when /Rack::MockRequest/
-          @request = RackMockRequest.new(request)
-      when /ActionDispatch::Request/
-        @request = ActionDispatchRequest.new(request)
+        @request = RequestDrivers::CurbRequest.new(request)
       else
         raise UnknownHTTPRequest, "#{request.class.to_s} is not yet supported."
       end
