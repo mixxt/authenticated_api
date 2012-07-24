05ffb4fa9c78497edcb864fedac041f0ba32748c
Christoph Geschwind <christoph@mixxt.net>
2012-07-24 08:11:37 +0000

    using sha256 instead of sha1, more signature specs

--- lib/authenticated_api/signature.rb
+++ lib/authenticated_api/signature.rb
@@ -2,6 +2,7 @@ module AuthenticatedApi
 
   class Signature < Struct.new(:method, :host, :uri, :params)
 
+    #http://docs.amazonwebservices.com/AmazonSimpleDB/latest/DeveloperGuide/HMACAuth.html
     #StringToSign = HTTPVerb + "\n" +
     #    ValueOfHostHeaderInLowercase + "\n" +
     #    HTTPRequestURI + "\n" +
@@ -15,6 +16,11 @@ module AuthenticatedApi
 
     def string_to_sign
       "#{method.upcase}\n#{host.downcase}\n#{uri}#{canonicalized_params}"
+    end
+
+    def sign_with(secret)
+      digest = OpenSSL::Digest::Digest.new('sha256')
+      Base64.encode64(OpenSSL::HMAC.digest(digest, secret, string_to_sign)).strip
     end
 
   end
--- spec/signature_spec.rb
+++ spec/signature_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
 describe AuthenticatedApi::Signature do
-
+  let(:secret) do
+    'secret'
+  end
   let(:signature) do
     signature = AuthenticatedApi::Signature.new('get', 'Example.com', '/', {'something' => 'value'})
+  end
+
+  let(:signed_string) do
+    AuthenticatedApi::Signature.new('get', 'Example.com', '/', {'something' => 'value'}).sign_with(secret)
   end
 
   it "stores method" do
@@ -25,4 +31,30 @@ describe AuthenticatedApi::Signature do
     signature.string_to_sign.should eq "GET\nexample.com\n/something=value"
   end
 
+  it "signs with secret" do
+    digest = OpenSSL::Digest::Digest.new('sha256')
+    ref = Base64.encode64(OpenSSL::HMAC.digest(digest, secret, "GET\nexample.com\n/something=value")).strip
+    puts signature.sign_with(secret).should eq ref
+  end
+
+  describe "signature changes" do
+
+    it "when method changes" do
+      signature.method = 'PUT'
+      signature.sign_with(secret).should_not eq signed_string
+    end
+    it "when host changes" do
+      signature.host = 'google.de'
+      signature.sign_with(secret).should_not eq signed_string
+    end
+    it "when uri changes" do
+      signature.uri = '/?foo=bar'
+      signature.sign_with(secret).should_not eq signed_string
+    end
+    it "when params changes" do
+      signature.params = {'new' => 'params'}
+      signature.sign_with(secret).should_not eq signed_string
+    end
+
+  end
 end
