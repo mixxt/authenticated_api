a67a82b82f739ba5b90daf009d4c486223d37188
Christoph Geschwind <christoph@mixxt.net>
2012-07-24 13:16:27 +0000

    using real request path instead of rack mounted path

--- lib/authenticated_api/server.rb
+++ lib/authenticated_api/server.rb
@@ -4,11 +4,12 @@ module AuthenticatedApi
     autoload :Middleware, 'authenticated_api/server/middleware'
 
     def self.valid_signature?(request, secret)
+      #puts "Comparing #{request.params['Signature'].inspect} with #{signature_for_request(request, secret)}"
       request.params['Signature'] == signature_for_request(request, secret)
     end
 
     def self.signature_for_request(request, secret)
-      Signature.new(request.request_method, request.host, request.path_info, request.params.except('Signature', 'AccessKeyID')).sign_with(secret)
+      Signature.new(request.request_method, request.host, request.env['REQUEST_PATH'], request.params.except('Signature', 'AccessKeyID')).sign_with(secret)
     end
   end
 
